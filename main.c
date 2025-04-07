69e872ad44b1dcc6665435dfe1e2979bf73d0994
Stepainpy <stepa2014vlg@gmail.com>
2025-04-08 01:01:33 +0300

    add byte match check

--- main.c
+++ main.c
@@ -22,6 +22,33 @@ void memdump(const void* src, size_t count, FILE* file) {
 } while (0);
 #endif
 
+uint8_t hexd2nib(char nib) {
+    if ('0' <= nib && nib <= '9')
+        return nib - '0';
+    if ('a' <= nib && nib <= 'f')
+        return nib - 'a' + 10;
+    return 0;
+}
+
+void check_hash(chl_ret_t* got) {
+    const size_t sz = sizeof *got;
+    uint8_t corp[sizeof *got] = {0};
+    for (size_t i = 0; i < sz; i++)
+        corp[i] =
+            hexd2nib(CORRECT_HASH[2 * i]) << 4 |
+            hexd2nib(CORRECT_HASH[2 * i + 1]);
+
+    uint8_t* cp = corp, *gp = (uint8_t*)got;
+    size_t match = 0;
+    for (size_t i = 0; i < sz; i++)
+        if (*cp++ == *gp++) ++match;
+
+    if (match == sz)
+        printf("\x1b[32mFull match\x1b[0m (%zu/%zu)\n", sz, sz);
+    else
+        printf("\x1b[31mIncomplete match\x1b[0m (%zu/%zu)\n", match, sz);
+}
+
 int main(void) {
     FILE* fd = fopen("text.txt", "rb");
     if (!fd) return 1;
@@ -40,6 +67,7 @@ int main(void) {
     puts("test: "CORRECT_HASH);
     puth("span", span_hash);
     puth("file", file_hash);
+    check_hash(&span_hash);
 
     free(buffer);
     fclose(fd);
